7b0e848485b84586687c9bb6425686e26b03c3a2
Joel Carlos <joelcarlos02@gmail.com>
2017-02-27 21:04:32 +0800

    npm start

--- web.js
+++ web.js
@@ -1,13 +1,16 @@
 var express = require('express')
   , http = require('http')
 	, app = express()
+//
+//
+// app.use(express.static(__dirname + '/dist'));
+//
+//
+// app.listen(process.env.PORT || 8000, function(){
+// 	console.log('Server running at http://127.0.0.1:8000/');
+// })
 
 
-app.use(express.static(__dirname + '/dist'));
-
-
-app.listen(process.env.PORT || 8000, function(){
-	console.log('Server running at http://127.0.0.1:8000/');
-})
-
-
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
